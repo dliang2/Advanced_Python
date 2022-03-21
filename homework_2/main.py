dca29a8260044aa63f37561f6f404ff49eb04993
Darren Liang <liangdarren2@gmail.com>
2022-03-21 09:08:53 -0400

    docstring

--- homework_2/main.py
+++ homework_2/main.py
@@ -1,19 +1,24 @@
 def generate_Kaprekar(end):
+    # iterate
     for i in range(end + 1):
+        # get the square
         sqr = i ** 2
-        digits = str(sqr)
+        digits = str(sqr) 
 
+        # and get the num of its digits
         length = len(digits)
         split = int(length/2)
 
         while length > 1:
+            # split the squares into two halves
             left = int(digits[:split])
             right = int(digits[split:])
+
+            # add the two halves to see if their sum is equal to the original number
             if (left + right) == i:
                 print("Number: " + str(i) + " Square: " + str(sqr))
             break
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     generate_Kaprekar(10000)
 
